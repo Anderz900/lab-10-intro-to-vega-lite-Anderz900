e7dde16bd472bf8d54e405fa79b9b79286b27b94
Andres Rivera <andres.r.rivera97@gmail.com>
2019-11-11 23:43:53 -0500

    final version

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 
-
-
 document.querySelector(".select-control").addEventListener("change", updateBarChart);
 
 function updateBarChart(){
   let column = document.querySelector(".select-control").value;
   let spec = {
-   
+    "title": "Coffee House Chains",
+    "data" : {"url": "data/coffee-house-chains.csv"},
+    "width": 500,
+    "mark": "bar",
+    "encoding":
+    {
+      "y":{"field":column,
+            "type":"quantitative"},
+      "x": {"field": "company", 
+            "type": "nominal",
+            "sort": "-y"}
+    }
   };
   vegaEmbed('#chart-area', spec);
 }
